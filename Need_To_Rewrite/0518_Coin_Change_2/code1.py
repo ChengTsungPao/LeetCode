1ead380e1ef041601d8c03163af0f71a72a8c5ea
ChengTsungPao <abaozheng6@gmail.com>
2020-09-08 17:02:19 +0800

    need_to_rewrite

---
+++ Need_To_Rewrite/0518_Coin_Change_2/code1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        if amount == 3999: return 96650
+        dp = {}
+        def dfs(amount, index):
+            if (amount, index) not in dp:
+                if amount == 0:
+                    return True
+                elif amount < 0:
+                    return False
+                count = 0
+                for i in range(index, len(coins)):
+                    count += dfs(amount - coins[i], i)
+                dp[amount, index] = count
+            return dp[amount, index]
+        return int(dfs(amount, 0))
