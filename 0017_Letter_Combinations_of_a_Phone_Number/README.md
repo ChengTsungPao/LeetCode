0cafef4d28000fe6f8e5213cddad291f6fb3a269
ChengTsungPao <abaozheng6@gmail.com>
2021-10-03 23:53:08 +0800

    leetcode_0017_0020_0024

--- 0017_Letter_Combinations_of_a_Phone_Number/README.md
+++ 0017_Letter_Combinations_of_a_Phone_Number/README.md
@@ -1,5 +1,11 @@
 ## Question: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
 code1.py:
-* Runtime: 28 ms, faster than 68.27% of Python3 online submissions for Letter Combinations of a Phone Number.
-* Memory Usage: 13.8 MB, less than 5.88% of Python3 online submissions for Letter Combinations of a Phone Number.
+* Runtime: 24 ms, faster than 96.72% of Python3 online submissions for Letter Combinations of a Phone Number.
+* Memory Usage: 14.3 MB, less than 61.84% of Python3 online submissions for Letter Combinations of a Phone Number.
+> math
+
+code2.py:
+* Runtime: 24 ms, faster than 96.72% of Python3 online submissions for Letter Combinations of a Phone Number.
+* Memory Usage: 14.3 MB, less than 30.60% of Python3 online submissions for Letter Combinations of a Phone Number.
+> recur
--- 0017_Letter_Combinations_of_a_Phone_Number/code1.py
+++ 0017_Letter_Combinations_of_a_Phone_Number/code1.py
@@ -1,28 +1,36 @@
 class Solution:    
     def letterCombinations(self, digits: str) -> List[str]:
-        if(digits==""):
-            ans = []
-        else:
-            data = {"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
-            def transform(num,long):
-                ans = []
-                index = len(digits)-1
-                while num!=0:
-                    tmp = len(data[digits[index]])
-                    ans.append(num%tmp)
-                    num = num//tmp
-                    index -= 1
-                for i in range(long-len(ans)):
-                    ans.append(0)
-                return ans
-            ans = []
-            length = 1
-            for i in digits:
-                length *= len(data[i])
-            for i in range(length):
-                t = transform(i,length)
-                s = ""
-                for j in range(len(digits)):
-                    s += data[digits[j]][t[len(digits)-j-1]]
-                ans.append(s)
+        
+        # 方法: 利用進位制的概念
+        # 範例: digits = "89" => "tw" = "00"、"tx" = "01" ... "vz" = "23"
+
+        table = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        
+        ans = []
+        maxNum = digits != ""
+        
+        for digit in digits:
+            maxNum *= len(table[digit])
+            
+        for num in range(maxNum):
+            s = ""
+            for digit in reversed(digits):
+                
+                divisor = len(table[digit])
+                quotient, remainder = num // divisor, num % divisor
+                
+                s = table[digit][remainder] + s
+                num = quotient
+                
+            ans.append(s)
+            
         return ans
