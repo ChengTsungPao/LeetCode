2b5afb0eb6cd24d5a97c96d9bcaa97ee7a523c41
chengtsungpao <abaozheng6@gmail.com>
2021-11-08 02:03:31 +0800

    leetcode_0079_0080_0084

--- 0079_Word_Search/code1.py
+++ 0079_Word_Search/code1.py
@@ -1,49 +1,30 @@
 class Solution:
-    def init(self):
-        self.map = []
     def exist(self, board: List[List[str]], word: str) -> bool:
-        import numpy as np
-        if(len(board)==0 and len(board[0])==0): 
-            return False
-        elif(len(board)==1 and len(board[0])==1): 
-            if(word==board[0][0]):
+        
+        def recur(i, j, index):
+            
+            if index == len(word):
                 return True
-            else:
+            
+            if not (0 <= i < len(board) and 0 <= j < len(board[0])) or board[i][j] != word[index]:
                 return False
-
-        def recur(s,x,y):
-            if(len(s)!=1):
-                if(y+1<len(board[0])):
-                    if(board[x][y+1]==s[1] and self.map[x][y+1]==1):
-                        self.map[x][y+1]=0
-                        if(recur(s[1:],x,y+1)):
-                            return True
-                        self.map[x][y+1]=1
-                if(x+1<len(board)):
-                    if(board[x+1][y]==s[1] and self.map[x+1][y]==1):
-                        self.map[x+1][y]=0
-                        if(recur(s[1:],x+1,y)):
-                            return True
-                        self.map[x+1][y]=1
-                if(x-1>=0):
-                    if(board[x-1][y]==s[1] and self.map[x-1][y]==1):
-                        self.map[x-1][y]=0
-                        if(recur(s[1:],x-1,y)):
-                            return True
-                        self.map[x-1][y]=1
-                if(y-1>=0):
-                    if(board[x][y-1]==s[1] and self.map[x][y-1]==1):
-                        self.map[x][y-1]=0
-                        if(recur(s[1:],x,y-1)):
-                            return True
-                        self.map[x][y-1]=1
-            else:
-                return True    
-              
+            
+            ch = board[i][j]
+            board[i][j] = "#"
+            
+            ret = recur(i + 1, j + 0, index + 1) or \
+                  recur(i + 0, j + 1, index + 1) or \
+                  recur(i - 1, j + 0, index + 1) or \
+                  recur(i + 0, j - 1, index + 1)
+            
+            board[i][j] = ch
+            
+            return ret
+        
+        
         for i in range(len(board)):
-            for j in range(len(board[0])): 
-                if(board[i][j]==word[0]):
-                    self.map = np.ones((len(board),len(board[0])))  
-                    self.map[i][j] = 0
-                    if(recur(word,i,j)): return True
+            for j in range(len(board[0])):
+                if recur(i, j, 0):
+                    return True
+                
         return False
