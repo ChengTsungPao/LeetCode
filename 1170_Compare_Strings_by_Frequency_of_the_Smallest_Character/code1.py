6c2a23d0909fda8581ff2d8c87d36b269cdf57e7
chengtsungpao <abaozheng6@gmail.com>
2021-11-28 22:26:14 +0800

    leetcode_1170_1232_1394

--- 1170_Compare_Strings_by_Frequency_of_the_Smallest_Character/code1.py
+++ 1170_Compare_Strings_by_Frequency_of_the_Smallest_Character/code1.py
@@ -1,11 +1,14 @@
 class Solution:
     def numSmallerByFrequency(self, queries: List[str], words: List[str]) -> List[int]:     
-        ans = collections.deque()
+        
         for i in range(len(words)):
             words[i] = words[i].count(min(words[i]))  
+            
         words.sort()
+        
+        ans = []
         for i in range(len(queries)):
             queries[i] = queries[i].count(min(queries[i]))
-            ans.append(len(words)-bisect.bisect_right(words,queries[i],0,len(words)))
+            ans.append(len(words) - bisect.bisect_right(words, queries[i]))
 
         return ans
