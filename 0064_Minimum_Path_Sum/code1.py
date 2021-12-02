41b45a42b0453d23297a82bdc1f5dc27aa3625b0
chengtsungpao <abaozheng6@gmail.com>
2021-12-02 11:09:58 +0800

    leetcode_0063_0064_0065

--- 0064_Minimum_Path_Sum/code1.py
+++ 0064_Minimum_Path_Sum/code1.py
@@ -1,9 +1,24 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if(i != 0 and j != 0):
-                    grid[i][j] += min(grid[i-1][j], grid[i][j-1])
-                else:
-                    grid[i][j] += max((i != 0) * grid[i-1][j], (j != 0) * grid[i][j-1])
+        '''
+        dp[i][j] => 到 (i, j) 的最小總合
+        
+        Init:
+            dp[0][i] = sum(dp[0][0 ~ i])
+            dp[j][0] = sum(dp[0 ~ j][0])
+        
+        Method:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + grid[i][]j
+        '''
+        
+        for i in range(1, len(grid)):
+            grid[i][0] += grid[i - 1][0]
+        
+        for j in range(1, len(grid[0])):
+            grid[0][j] += grid[0][j - 1]
+            
+        for i in range(1, len(grid)):
+            for j in range(1, len(grid[0])):
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+                
         return grid[-1][-1]
