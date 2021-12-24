06b45da9a7e612fb585ac8ba1f2f6669cfae0949
chengtsungpao <abaozheng6@gmail.com>
2021-12-25 04:27:33 +0800

    leetcode_0037_0038_0057

--- 0057_Insert_Interval/README.md
+++ 0057_Insert_Interval/README.md
@@ -1,5 +1,9 @@
 ## Question: https://leetcode.com/problems/insert-interval/
 
 code1.py:
-* Runtime: 72 ms, faster than 97.18% of Python3 online submissions for Insert Interval.
-* Memory Usage: 17.3 MB, less than 100.00% of Python3 online submissions for Insert Interval.
+* Runtime: 80 ms, faster than 71.07% of Python3 online submissions for Insert Interval.
+* Memory Usage: 18.7 MB, less than 32.32% of Python3 online submissions for Insert Interval.
+
+code2.py:
+* Runtime: 72 ms, faster than 95.84% of Python3 online submissions for Insert Interval.
+* Memory Usage: 17.4 MB, less than 92.12% of Python3 online submissions for Insert Interval.
--- 0057_Insert_Interval/code1.py
+++ 0057_Insert_Interval/code1.py
@@ -1,20 +1,22 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        record = [(newInterval[0], 1), (newInterval[1], -1)]
+        
+        for start, end in intervals:
+            record.append((start, 1))
+            record.append((end, -1))
+              
+        
         ans = []
-        Interval = []
-        for i in range(len(intervals)):
-            for _ in range(len(newInterval)):
-                if intervals[i][0] <= newInterval[0] <= intervals[i][1]:
-                    Interval.append(intervals[i][-len(newInterval)])
-                    del newInterval[0]
-                    flag = True
-                elif newInterval[0] <= intervals[i][0]:
-                    Interval.append(newInterval[0])
-                    del newInterval[0]
-                    flag = False
-            if len(Interval) == 2:
-                ans += [Interval] + intervals[i + flag:]
-                break
-            if len(Interval) != 1:
-                ans += [intervals[i]]
-        return ans + [Interval + newInterval] * (len(Interval) != 2 or intervals == [])
+        current = 0
+
+        for time, target in sorted(record, key = lambda x: (x[0], -x[1])):
+            if current == 0:
+                ans.append([time])
+        
+            current += target
+            if current == 0:
+                ans[-1].append(time)
+                
+        return ans
