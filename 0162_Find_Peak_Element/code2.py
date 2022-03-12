a9befd70389f3c62c77d519ac687d77673787f0b
chengtsungpao <abaozheng6@gmail.com>
2022-03-13 02:17:58 +0800

    leetcode_0152_0155_0162

--- 0162_Find_Peak_Element/code2.py
+++ 0162_Find_Peak_Element/code2.py
@@ -1,11 +1,14 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
+        
         left = 0
         right = len(nums) - 1
-        while left != right:
-            mid = (left + right) // 2
-            if nums[mid] > nums[mid + 1]:
-                right = mid
-            else:
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] < nums[mid + 1]:
                 left = mid + 1
+            else:
+                right = mid
+                
         return left
