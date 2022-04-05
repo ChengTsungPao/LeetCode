246857cf3971bf3a2ddd46930eaad98b3ef7b40c
chengtsungpao <abaozheng6@gmail.com>
2022-04-05 17:10:16 +0800

    leetcode_0322_0518_0877

--- 0322_Coin_Change/code2.cpp
+++ 0322_Coin_Change/code2.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int dp[10001] = {0};
-        std::sort(coins.begin(), coins.end());
 
         for(int i = 1; i <= amount; i++){
             dp[i] = INT_MAX;   
 
-            for(int j = 0; j < coins.size() && coins[j] <= i; j++){
-                dp[i] = min(dp[i], dp[i - coins[j]]);
+            for(int j = 0; j < coins.size(); j++){
+                if(i - coins[j] >= 0){
+                    dp[i] = min(dp[i], dp[i - coins[j]]);
+                }
             }
             
             dp[i] += dp[i] != INT_MAX;
