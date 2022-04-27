ca039ab0ad7d3e1185d45b0b1b3c832f77aad7fa
chengtsungpao <abaozheng6@gmail.com>
2022-04-27 21:18:00 +0800

    leetcode_0111_0113_0116

--- 0116_Populating_Next_Right_Pointers_in_Each_Node/README.md
+++ 0116_Populating_Next_Right_Pointers_in_Each_Node/README.md
@@ -3,4 +3,9 @@
 code1.py:
 * Runtime: 68 ms, faster than 44.85% of Python3 online submissions for Populating Next Right Pointers in Each Node.
 * Memory Usage: 15.3 MB, less than 57.14% of Python3 online submissions for Populating Next Right Pointers in Each Node.
->bfs
+> bfs
+
+code1.py:
+* Runtime: 66 ms, faster than 83.45% of Python3 online submissions for Populating Next Right Pointers in Each Node.
+* Memory Usage: 15.5 MB, less than 97.02% of Python3 online submissions for Populating Next Right Pointers in Each Node.
+> bfs
---
+++ 0116_Populating_Next_Right_Pointers_in_Each_Node/code2.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        
+        que = collections.deque([root])
+        while que:
+            
+            newQue = collections.deque()
+            while que:
+                node = que.pop()
+                
+                if que:
+                    node.next = que[-1]
+                    
+                for nextNode in [node.left, node.right]:
+                    if not nextNode:
+                        continue
+                        
+                    newQue.appendleft(nextNode)
+                    
+            que = newQue
+            
+        return root
