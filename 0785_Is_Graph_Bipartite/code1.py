e034bc1661f19c3e87a9477369c2bf8b6c77798e
chengtsungpao <abaozheng6@gmail.com>
2022-05-04 05:29:55 +0800

    leetcode_0269_0785_0934

--- 0785_Is_Graph_Bipartite/code1.py
+++ 0785_Is_Graph_Bipartite/code1.py
@@ -1,29 +1,29 @@
 class Solution:
-    def isBipartite(self, graph: List[List[int]]) -> bool:        
-        colors = {}
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        color = {}
         
         def bfs(node):
-            if node in colors:
+            if node in color:
                 return True
             
-            que = collections.deque([(node, True)])
+            que = collections.deque([node])
+            color[node] = True
             while que:
-                node, color = que.pop()
-                colors[node] = color
-
+                node = que.pop()
+                
                 for nextNode in graph[node]:
-                    if nextNode in colors:
-                        if colors[nextNode] == color:
+                    if nextNode in color:
+                        if color[nextNode] == color[node]:
                             return False
-                        else:
-                            continue
-                    else:
-                        que.appendleft((nextNode, not color))
-                        
+                        continue
+                    
+                    color[nextNode] = not color[node]
+                    que.appendleft(nextNode)
+                    
             return True
-                        
+        
         for node in range(len(graph)):
             if bfs(node) == False:
                 return False
-                    
+            
         return True
