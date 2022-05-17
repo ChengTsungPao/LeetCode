16b53da1dc0ec0f5b8f610cd2f213584cfb1edf4
chengtsungpao <abaozheng6@gmail.com>
2022-05-17 08:48:23 +0800

    leetcode_0456_0841_0875

--- 0456_132_Pattern/code3.py
+++ 0456_132_Pattern/code3.py
@@ -5,13 +5,11 @@ class Solution:
         if n < 3:
             return False
         
-        
         preMin = [0] * n
         preMin[0] = nums[0]
         for i in range(1, n):
             preMin[i] = min(preMin[i - 1], nums[i])
             
-        
         stack = []
         for j in range(n - 1, 0, -1):
             if preMin[j - 1] > nums[j]:
