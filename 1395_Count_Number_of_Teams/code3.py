db304c3473951ba86a751cc65aa328fd879bd508
chengtsungpao <abaozheng6@gmail.com>
2022-05-24 01:08:08 +0800

    leetcode_1395_1396_1466

--- 1395_Count_Number_of_Teams/code3.py
+++ 1395_Count_Number_of_Teams/code3.py
@@ -5,26 +5,24 @@ class Solution:
         n = len(rating)
         
         leftSamller = [0] * n
-        leftBigger = [0] * n
         rightSamller = [0] * n
-        rightBigger = [0] * n
         
         bst = SortedList([rating[0]])
         for i in range(1, n):
             count = bst.bisect_left(rating[i])
             leftSamller[i] = count
-            leftBigger[i] = len(bst) - count
             bst.add(rating[i])
             
         bst = SortedList([rating[-1]])
         for i in range(n - 2, -1, -1):
             count = bst.bisect_left(rating[i])
             rightSamller[i] = count
-            rightBigger[i] = len(bst) - count
             bst.add(rating[i])
         
         ans = 0
         for i in range(n):
-            ans += leftSamller[i] * rightBigger[i] + leftBigger[i] * rightSamller[i]
+            leftBiggerCount = i - leftSamller[i]
+            rightBiggerCount = (n - i - 1) - rightSamller[i]
+            ans += leftSamller[i] * rightBiggerCount + leftBiggerCount * rightSamller[i]
 
         return ans
