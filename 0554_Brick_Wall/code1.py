a304aaaf2922b75c7c581fe52f508a8001e91559
chengtsungpao <abaozheng6@gmail.com>
2022-06-11 05:34:32 +0800

    leetcode_0068_0072_0554

--- 0554_Brick_Wall/code1.py
+++ 0554_Brick_Wall/code1.py
@@ -1,13 +1,13 @@
 class Solution:
     def leastBricks(self, wall: List[List[int]]) -> int:
         
-        count = collections.defaultdict(int)
-        count[0] = 0
+        n = len(wall)
         
-        for i in range(len(wall)):
-            s = wall[i][0]
-            for j in range(1, len(wall[i])):
-                count[s] += 1
-                s += wall[i][j]
-
-        return len(wall) - max(count.values())
+        count = collections.defaultdict(int)
+        for i in range(n):
+            width = 0
+            for j in range(len(wall[i]) - 1):
+                width += wall[i][j]
+                count[width] += 1
+                
+        return n - max(count.values(), default = 0)
