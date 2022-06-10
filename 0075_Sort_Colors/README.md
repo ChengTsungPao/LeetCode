a07735abd53a889f99f4d826664f00b1d2bff5fe
chengtsungpao <abaozheng6@gmail.com>
2022-06-11 03:47:46 +0800

    leetcode_0075_0077_0078

--- 0075_Sort_Colors/README.md
+++ 0075_Sort_Colors/README.md
@@ -6,6 +6,11 @@ code1.py:
 > space: O(N)
 
 code2.py:
-* Runtime: 28 ms, faster than 92.29% of Python3 online submissions for Sort Colors.
-* Memory Usage: 14.3 MB, less than 46.13% of Python3 online submissions for Sort Colors.
+* Runtime: 31 ms, faster than 95.52% of Python3 online submissions for Sort Colors.
+* Memory Usage: 13.8 MB, less than 62.80% of Python3 online submissions for Sort Colors.
+> space: O(1)
+
+code3.py:
+* Runtime: 33 ms, faster than 92.04% of Python3 online submissions for Sort Colors.
+* Memory Usage: 13.9 MB, less than 14.38% of Python3 online submissions for Sort Colors.
 > space: O(1)
--- 0075_Sort_Colors/code2.py
+++ 0075_Sort_Colors/code2.py
@@ -1,16 +1,15 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         
-        i, j = 0, len(nums) - 1
-        k = 0
-        while k <= j:
-            if nums[k] == 0:
-                nums[i], nums[k] = nums[k], nums[i]
+        n = len(nums)
+        
+        i = 0
+        for j in range(n):
+            if nums[j] == 0:
+                nums[i], nums[j] = nums[j], nums[i]
                 i += 1
-                k = max(i, k)  # 避免i超過k造成已經排好的0換到錯誤的位置
-            elif nums[k] == 2:
-                nums[j], nums[k] = nums[k], nums[j]
-                j -= 1
-                k += 0         # 若交換後是0或2，k不可以前進
-            else:
-                k += 1         # 若遇到1略過
+                
+        for j in range(i, n):
+            if nums[j] == 1:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
