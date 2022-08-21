5c52ccedc8d07787aa34e9789e1f3dc6ceaaa05b
chengtsungpao <abaozheng6@gmail.com>
2022-08-22 01:01:46 +0800

    G-M

---
+++ 2017_Grid_Game/README.md
@@ -0,0 +1,10 @@
+## Question: https://leetcode.com/problems/grid-game/
+
+code1.py:
+* Time Limit Exceeded
+> prefixSum + suffixSum
+
+code2.py:
+* Runtime: 2753 ms, faster than 5.21% of Python3 online submissions for Grid Game.
+* Memory Usage: 31 MB, less than 8.09% of Python3 online submissions for Grid Game.
+> prefixSum + suffixSum
---
+++ 2017_Grid_Game/code2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        
+        n = len(grid[0])
+        
+        prefixSum = copy.deepcopy(grid)
+        suffixSum = copy.deepcopy(grid)
+        
+        for i in range(2):
+            for j in range(1, n):
+                prefixSum[i][ j] += prefixSum[i][ j - 1]
+                suffixSum[i][~j] += suffixSum[i][~j + 1]
+        
+        # cut1 <= cut2 => prefixSum[0][cut2] - prefixSum[0][cut1]
+        # cut1 >= cut2 => suffixSum[1][cut1] - suffixSum[1][cut2]
+        
+        ans = float("inf")
+        for cut1 in range(n):
+            maxScore = 0
+            for cut2 in [0, n - 1]:
+                if cut1 == cut2:
+                    continue
+
+                if cut1 <= cut2:
+                    maxScore = max(maxScore, prefixSum[0][cut2] - prefixSum[0][cut1])
+                else:
+                    maxScore = max(maxScore, suffixSum[1][cut2] - suffixSum[1][cut1])
+
+            ans = min(ans, maxScore)
+        
+        return ans
