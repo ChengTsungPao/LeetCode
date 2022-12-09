f7b4a3f9d65cd809a65fdd4545f36574f0fdfc90
chengtsungpao <abaozheng6@gmail.com>
2022-12-09 14:13:11 +0800

    daily c++

---
+++ 1026_Maximum_Difference_Between_Node_and_Ancestor/README.md
@@ -0,0 +1,6 @@
+## Question: https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
+
+code1.cpp:
+* Runtime: 11 ms, faster than 50.39% of C++ online submissions for Maximum Difference Between Node and Ancestor.
+* Memory Usage: 11.4 MB, less than 18.84% of C++ online submissions for Maximum Difference Between Node and Ancestor.
+> recur
---
+++ 1026_Maximum_Difference_Between_Node_and_Ancestor/code1.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxAncestorDiff(TreeNode* root) {
+        int ans = 0;
+        recur(root, ans);
+        return ans;
+    }
+    
+    tuple<int, int> recur(TreeNode* root, int& ans){
+        if (root -> left == nullptr && root -> right == nullptr){
+            return make_tuple(root -> val, root -> val);
+        } 
+        
+        tuple<int, int> pair1 = (root -> left  != nullptr) ? recur(root -> left,  ans) : make_tuple(0, 100000);
+        tuple<int, int> pair2 = (root -> right != nullptr) ? recur(root -> right, ans) : make_tuple(0, 100000);
+        int _max = max(get<0>(pair1), get<0>(pair2));
+        int _min = min(get<1>(pair1), get<1>(pair2));
+        
+        ans = max(ans, max(abs(root -> val - _max), abs(root -> val - _min))); 
+        return make_tuple(max(root -> val, _max), min(root -> val, _min));      
+    }
+};
