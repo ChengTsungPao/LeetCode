7cebee744077bd8b38a3621aca8b04b0335c2cb7
chengtsungpao <abaozheng6@gmail.com>
2022-12-09 14:15:43 +0800

    daily c++

--- 1026_Maximum_Difference_Between_Node_and_Ancestor/README.md
+++ 1026_Maximum_Difference_Between_Node_and_Ancestor/README.md
@@ -1,6 +1,6 @@
 ## Question: https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
 
 code1.cpp:
-* Runtime: 11 ms, faster than 50.39% of C++ online submissions for Maximum Difference Between Node and Ancestor.
-* Memory Usage: 11.4 MB, less than 18.84% of C++ online submissions for Maximum Difference Between Node and Ancestor.
+* Runtime: 3 ms, faster than 97.10% of C++ online submissions for Maximum Difference Between Node and Ancestor.
+* Memory Usage: 11.5 MB, less than 17.61% of C++ online submissions for Maximum Difference Between Node and Ancestor.
 > recur
--- 1026_Maximum_Difference_Between_Node_and_Ancestor/code1.cpp
+++ 1026_Maximum_Difference_Between_Node_and_Ancestor/code1.cpp
@@ -22,10 +22,10 @@ public:
             return make_tuple(root -> val, root -> val);
         } 
         
-        tuple<int, int> pair1 = (root -> left  != nullptr) ? recur(root -> left,  ans) : make_tuple(0, 100000);
-        tuple<int, int> pair2 = (root -> right != nullptr) ? recur(root -> right, ans) : make_tuple(0, 100000);
-        int _max = max(get<0>(pair1), get<0>(pair2));
-        int _min = min(get<1>(pair1), get<1>(pair2));
+        tuple<int, int> tuple1 = (root -> left  != nullptr) ? recur(root -> left,  ans) : make_tuple(0, 100000);
+        tuple<int, int> tuple2 = (root -> right != nullptr) ? recur(root -> right, ans) : make_tuple(0, 100000);
+        int _max = max(get<0>(tuple1), get<0>(tuple2));
+        int _min = min(get<1>(tuple1), get<1>(tuple2));
         
         ans = max(ans, max(abs(root -> val - _max), abs(root -> val - _min))); 
         return make_tuple(max(root -> val, _max), min(root -> val, _min));      
