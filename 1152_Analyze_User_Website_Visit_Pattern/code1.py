a10d6ef1ef6e5ebd33d517ea858ca732e3b2b696
chengtsungpao <abaozheng6@gmail.com>
2023-01-09 02:54:17 +0800

    A_M

--- 1152_Analyze_User_Website_Visit_Pattern/code1.py
+++ 1152_Analyze_User_Website_Visit_Pattern/code1.py
@@ -5,16 +5,16 @@ class Solution:
         for _, person, web in sorted(zip(timestamp, username, website)):
             personVisited[person].append(web)
 
-        def recur(i, vistedTuple, websites):
-            if len(vistedTuple) == 3:
-                return {vistedTuple}
+        def recur(i, visitedTuple, websites):
+            if len(visitedTuple) == 3:
+                return {visitedTuple}
             elif i == len(websites):
                 return set()
-            return recur(i + 1, vistedTuple + (websites[i],), websites) | recur(i + 1, vistedTuple, websites)
+            return recur(i + 1, visitedTuple + (websites[i],), websites) | recur(i + 1, visitedTuple, websites)
             
         count = collections.Counter()
         for person, websites in personVisited.items():
-            for vistedTuple in recur(0, (), websites):
-                count[vistedTuple] += 1
+            for visitedTuple in recur(0, (), websites):
+                count[visitedTuple] += 1
 
-        return list(min([(-times, vistedTuple) for vistedTuple, times in count.items()])[1])
+        return list(min([(-times, visitedTuple) for visitedTuple, times in count.items()])[1])
