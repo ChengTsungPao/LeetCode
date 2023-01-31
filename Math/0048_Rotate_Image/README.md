7a58092693b1c1ec123f48436d1062286d3c889a
chengtsungpao <abaozheng6@gmail.com>
2023-01-31 20:25:13 +0800

    review_20230131

---
+++ Math/0048_Rotate_Image/README.md
@@ -0,0 +1,5 @@
+## Question: https://leetcode.com/problems/rotate-image/
+###### tags: `LC0048`
+
+* Time complexity: O(mn)
+* Space complexity: O(1)
---
+++ Math/0048_Rotate_Image/code1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        for i in range(m // 2):
+            for j in range(n):
+                matrix[i][j], matrix[~i][j] = matrix[~i][j], matrix[i][j]
+        
+        for i in range(m):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
