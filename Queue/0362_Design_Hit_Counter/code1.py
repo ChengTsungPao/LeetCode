74474b68ba117c49203ec40193a24a016a4d732b
chengtsungpao <abaozheng6@gmail.com>
2023-02-02 00:35:29 +0800

    review_20230202

--- Queue/0362_Design_Hit_Counter/code1.py
+++ Queue/0362_Design_Hit_Counter/code1.py
@@ -1,15 +1,22 @@
-class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+class HitCounter:
 
-        ans = []
-        que = collections.deque()
-        for i, num in enumerate(nums):
-            while que and num > que[-1][0]:
-                que.pop()
-            que.append((num, i))
-            while que and que[0][1] <= i - k:
-                que.popleft()
-            if i >= k - 1:
-                ans.append(que[0][0])
-            
-        return ans
+    def __init__(self):
+        self.que = collections.deque()
+        
+    def adjust(self, timestamp):
+        while self.que and timestamp - self.que[0] + 1 > 300:
+            self.que.popleft()
+
+    def hit(self, timestamp: int) -> None:
+        self.adjust(timestamp)
+        self.que.append(timestamp)
+        
+    def getHits(self, timestamp: int) -> int:
+        self.adjust(timestamp)
+        return len(self.que)
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
