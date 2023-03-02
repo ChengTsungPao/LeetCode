3e24f7334ba753bdb8861953cb8d8ef6dda12924
chengtsungpao <abaozheng6@gmail.com>
2023-03-03 03:27:20 +0800

    week_hard

--- 1526_Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array/code3.py
+++ 1526_Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array/code3.py
@@ -5,6 +5,7 @@ class Solution:
          >_|_| > |_|    _
         >|_|_|_>_|_|_>_|_|
         >|_|_|_|_|_|_|_|_|
+         [2,3,1,1,3,1,1,2]
         
         '''
         return target[0] + sum([max(target[i] - target[i - 1], 0) for i in range(1, len(target))])
