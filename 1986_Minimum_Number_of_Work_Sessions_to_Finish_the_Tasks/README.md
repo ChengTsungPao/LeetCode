3a67b759ffef06fbf426008e97b283cf2b56e2b2
chengtsungpao <abaozheng6@gmail.com>
2023-03-20 01:51:24 +0800

    c++

---
+++ 1986_Minimum_Number_of_Work_Sessions_to_Finish_the_Tasks/README.md
@@ -0,0 +1,6 @@
+## Question: https://leetcode.com/problems/minimum-number-of-work-sessions-to-finish-the-tasks/
+
+code1.cpp:
+* Runtime: 54 ms, faster than 90.53% of C++ online submissions for Minimum Number of Work Sessions to Finish the Tasks.
+* Memory Usage: 8.5 MB, less than 87.50% of C++ online submissions for Minimum Number of Work Sessions to Finish the Tasks.
+> recur
---
+++ 1986_Minimum_Number_of_Work_Sessions_to_Finish_the_Tasks/code1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int ans = INT_MAX;
+    
+    int minSessions(vector<int>& tasks, int sessionTime) {
+        vector<int> buckets;
+        recur(0, buckets, tasks, sessionTime);
+        return ans;
+    }
+    
+    void recur(int idx, vector<int>& buckets, vector<int>& tasks, int sessionTime){
+        
+        // puring
+        if(buckets.size() >= ans){
+            return;
+        }
+        
+        // stop condition
+        if(idx >= tasks.size()){
+            ans = buckets.size();
+            return;
+        }
+        
+        int time = tasks[idx];
+        
+        // choose one bucket in buckets
+        for(int i = 0; i < buckets.size(); i++){
+            if(time + buckets[i] <= sessionTime){
+                buckets[i] += time;
+                recur(idx + 1, buckets, tasks, sessionTime);
+                buckets[i] -= time;
+            }
+        }
+        
+        // add one bucket in buckets
+        buckets.push_back(time);
+        recur(idx + 1, buckets, tasks, sessionTime);
+        buckets.pop_back();
+    }
+};
