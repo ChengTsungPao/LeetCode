7a1d2700d2c443283f310cf0033c9ea79febf7eb
chengtsungpao <abaozheng6@gmail.com>
2023-03-24 20:59:17 +0800

    daily c++

--- 1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero/README.md
+++ 1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero/README.md
@@ -14,3 +14,8 @@ code3.cpp:
 * Runtime: 760 ms, faster than 16.38% of C++ online submissions for Reorder Routes to Make All Paths Lead to the City Zero.
 * Memory Usage: 207.2 MB, less than 5.05% of C++ online submissions for Reorder Routes to Make All Paths Lead to the City Zero.
 > dfs
+
+code4.cpp:
+* Runtime: 421 ms, faster than 59.35% of C++ online submissions for Reorder Routes to Make All Paths Lead to the City Zero.
+* Memory Usage: 129.4 MB, less than 30.44% of C++ online submissions for Reorder Routes to Make All Paths Lead to the City Zero.
+> dfs + mark
---
+++ 1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero/code4.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        
+        // create graph
+        unordered_map<int, vector<tuple<int, int>>> graph;
+        vector<int> graph_n(n, -1);
+        for(int i = 0; i < connections.size(); i++){
+            int node1 = connections[i][0], node2 = connections[i][1];
+            graph[node2].push_back({node1, 0});
+            graph[node1].push_back({node2, 1});
+        }
+        
+        // traversal tree
+        vector<int> visited(n, false);
+        visited[0] = true;
+        return recur(0, visited, graph);
+    }
+    
+    int recur(int node, vector<int>& visited, unordered_map<int, vector<tuple<int, int>>>& graph){
+        int ans = 0;
+        for(auto& [nextNode, target]: graph[node]){
+            if(!visited[nextNode]){
+                visited[nextNode] = true;
+                ans += target + recur(nextNode, visited, graph);
+            }
+        }
+        return ans;
+    }
+};
