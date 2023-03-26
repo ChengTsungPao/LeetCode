fe1bab8401d8d639f612e855b1fd4d772d80dfd9
chengtsungpao <abaozheng6@gmail.com>
2023-03-27 04:14:02 +0800

    daily c++

---
+++ 2360_Longest_Cycle_in_a_Graph/README.md
@@ -0,0 +1,5 @@
+## Question: https://leetcode.com/problems/longest-cycle-in-a-graph/
+
+code1.cpp:
+* Runtime: 556 ms, faster than 30.50% of C++ online submissions for Longest Cycle in a Graph.
+* Memory Usage: 201.7 MB, less than 21.16% of C++ online submissions for Longest Cycle in a Graph.
---
+++ 2360_Longest_Cycle_in_a_Graph/code1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int ans = -1;
+    
+    int longestCycle(vector<int>& edges) {
+        int n = edges.size();
+        vector<bool> cache(n, false);
+        
+        for(int node = 0; node < n; node++){
+            unordered_map<int, int> targets;
+            recur(node, 0, targets, cache, edges);
+            
+            for(auto it = targets.begin(); it != targets.end(); it++){
+                cache[it -> first] = true;
+            }
+        }
+        return ans;
+    }
+    
+    void recur(int node, int target, unordered_map<int, int>& targets, vector<bool>& cache, vector<int>& edges){
+        int nextNode = edges[node];
+        
+        if(cache[node] || nextNode == -1){
+            return;
+        } else if(targets.find(nextNode) != targets.end()){
+            int cycle = target - targets[nextNode] + 1;
+            ans = (ans < cycle && cycle != 0) ? cycle : ans;
+            return;
+        }
+        
+        targets[node] = target;
+        recur(nextNode, target + 1, targets, cache, edges);
+    }
+};
