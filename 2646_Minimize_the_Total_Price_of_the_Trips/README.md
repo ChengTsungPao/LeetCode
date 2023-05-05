d65d7a6ef64b110d91503171a344a5e7c538b172
chengtsungpao <abaozheng6@gmail.com>
2023-05-05 14:21:13 +0800

    daily hard

---
+++ 2646_Minimize_the_Total_Price_of_the_Trips/README.md
@@ -0,0 +1,6 @@
+## Question: https://leetcode.com/problems/minimize-the-total-price-of-the-trips/
+
+code1.py:
+* Runtime: 256 ms, faster than 59.39% of Python3 online submissions for Minimize the Total Price of the Trips.
+* Memory Usage: 16.7 MB, less than 5.33% of Python3 online submissions for Minimize the Total Price of the Trips.
+> recur + memo
---
+++ 2646_Minimize_the_Total_Price_of_the_Trips/code1.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minimumTotalPrice(self, n: int, edges: List[List[int]], price: List[int], trips: List[List[int]]) -> int:
+        
+        graph = collections.defaultdict(list)
+        for node1, node2 in edges:
+            graph[node1].append(node2)
+            graph[node2].append(node1)
+        
+        nodeVisitedTimes = collections.defaultdict(int)
+        def countNodeTimes(node, preNode, end):
+            if node == end:
+                return True
+            for nextNode in graph[node]:
+                if nextNode != preNode:
+                    nodeVisitedTimes[nextNode] += 1
+                    if countNodeTimes(nextNode, node, end):
+                        return True
+                    nodeVisitedTimes[nextNode] -= 1
+            return False
+        
+        # get node visited times
+        for start, end in trips: 
+            nodeVisitedTimes[start] += 1
+            countNodeTimes(start, -1, end)
+        
+        # get min cost
+        @functools.lru_cache(None)
+        def getMinCost(node, preNode, state):
+            p, t = price[node], nodeVisitedTimes[node] 
+            
+            if len(graph[node]) == 1 and graph[node][0] == preNode:
+                return t * p // 2 if not state else t * p
+            
+            select = t * p // 2
+            noSelect = t * p
+            for nextNode in graph[node]:
+                if nextNode == preNode:
+                    continue
+                noSelect += getMinCost(nextNode, node, False)
+                if not state: select += getMinCost(nextNode, node, True)
+            return min(select, noSelect) if not state else noSelect
+        
+        return getMinCost(0, -1, False)
