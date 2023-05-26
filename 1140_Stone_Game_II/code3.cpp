6bf211612a6d504249ad53f3b3b491da69c8756e
chengtsungpao <abaozheng6@gmail.com>
2023-05-26 11:07:28 +0800

    daily c++

---
+++ 1140_Stone_Game_II/code3.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        int _sum = accumulate(piles.begin(), piles.end(), 0);
+        vector<vector<vector<int>>> memo(n, vector<vector<int>>(n + 1, vector<int>(2, -1)));
+        return (_sum + recur(0, 1, true, piles, memo)) / 2;
+    }
+    
+    int recur(int idx, int M, bool who, vector<int>& piles, vector<vector<vector<int>>>& memo) {
+        int n = piles.size();
+        if(idx >= n){
+            return 0;
+        }
+        
+        if(memo[idx][M][who] != -1){
+            return memo[idx][M][who];
+        }
+        
+        int ans = (who) ? INT_MIN : INT_MAX, _sum = 0;
+        if(who){
+            for(int x = 1; x <= min(2 * M, n - idx); x++){
+                _sum += piles[idx + x - 1];
+                ans = max(ans, recur(idx + x, max(x, M), false, piles, memo) + _sum);
+            }
+        } else {
+            for(int x = 1; x <= min(2 * M, n - idx); x++){
+                _sum += piles[idx + x - 1];
+                ans = min(ans, recur(idx + x, max(x, M), true, piles, memo) - _sum);
+            }
+        }
+        
+        memo[idx][M][who] = ans;
+        return ans;
+    }
+    
+};
---
+++ 1140_Stone_Game_II/code4.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        int _sum = accumulate(piles.begin(), piles.end(), 0);
+        vector<vector<int>> memo(n, vector<int>(n + 1, -1));
+        return (_sum + recur(0, 1, piles, memo)) / 2;
+    }
+    
+    int recur(int idx, int M, vector<int>& piles, vector<vector<int>>& memo) {
+        int n = piles.size();
+        if(idx >= n){
+            return 0;
+        }
+        
+        if(memo[idx][M] != -1){
+            return memo[idx][M];
+        }
+        
+        int ans = INT_MIN, _sum = 0;
+        for(int x = 1; x <= min(2 * M, n - idx); x++){
+            _sum += piles[idx + x - 1];
+            ans = max(ans, _sum - recur(idx + x, max(x, M), piles, memo));
+        }
+        
+        memo[idx][M] = ans;
+        return ans;
+    }
+    
+};
