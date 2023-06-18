2e0d413abd3a242236bf448bd744a8c7e02fd0a6
chengtsungpao <abaozheng6@gmail.com>
2023-06-19 04:31:17 +0800

    daily c++

---
+++ 2328_Number_of_Increasing_Paths_in_a_Grid/README.md
@@ -0,0 +1,6 @@
+## Question: https://leetcode.com/problems/number-of-increasing-paths-in-a-grid/
+
+code1.cpp:
+* Runtime: 299 ms, faster than 66.95% of C++ online submissions for Number of Increasing Paths in a Grid.
+* Memory Usage: 43.4 MB, less than 80.67% of C++ online submissions for Number of Increasing Paths in a Grid.
+> recur + memo
---
+++ 2328_Number_of_Increasing_Paths_in_a_Grid/code1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int MOD = pow(10, 9) + 7;
+    
+    int countPaths(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        int ans = 0;
+        vector<vector<int>> count(m, vector<int>(n, -1));
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                recur(i, j, 0, count, grid);
+                ans = (ans + count[i][j]) % MOD;
+            }
+        }
+        
+        return ans;
+    }
+    
+    int recur(int i, int j, int pre, vector<vector<int>>& count, vector<vector<int>>& grid){
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        if(i >= m || i < 0 || j >= n || j < 0 || pre >= grid[i][j]) {
+            return 0;
+        }
+        
+        if(count[i][j] != -1){
+            return count[i][j];
+        }
+        
+        int ans = 1;
+        
+        ans = (ans + recur(i + 1, j, grid[i][j], count, grid)) % MOD;
+        ans = (ans + recur(i, j + 1, grid[i][j], count, grid)) % MOD;
+        ans = (ans + recur(i - 1, j, grid[i][j], count, grid)) % MOD;
+        ans = (ans + recur(i, j - 1, grid[i][j], count, grid)) % MOD;
+        
+        count[i][j] = ans;
+        return ans;
+    }
+};
